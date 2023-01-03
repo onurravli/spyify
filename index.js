aafd4e299171b3ab2748a70f66a1ba237f97aee5
onurravli <onurravli@hotmail.com>
2023-01-03 11:54:17 +0300

    Update node_modules

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello from spyify");
+const chalk = require('chalk')
+const buddyList = require('spotify-buddylist')
+let SP_DC = ""
+
+if (process.env.SP_DC) {
+    SP_DC = process.env.SP_DC
+}
+
+const main = async () => {
+    console.log("Hello from spyify.")
+}
+
+main()
